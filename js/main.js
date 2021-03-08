488b25f078b5fd008608b8ff2e9d92df7efc9d53
premsharma1230 <60520468+premsharma1230@users.noreply.github.com>
2021-03-08 18:21:37 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -23,6 +23,94 @@ $(document).ready(function(){
 //   $(".checkbox").click(function(){
 //        $(".b1").fadeToggle("");
 //   })
+  // add & Remove class
+  // $(".blank-heart").click(function () {
+  //   if(!$(this).hasClass('heart-active'))
+  //   {    
+  //       $(".blank-heart.heart-active").removeClass("heart-active");
+  //        $(".blank-heart.heart-active").css("transition","all .4s ease-in-out");
+  //       $(this).addClass("heart-active");  
+  //       $(this).css("transition","all .4s ease-in-out");      
+  //   }
+  // });
+
+//-----------heart------//
+
+  $(".blank-heart").click(function(){
+       $(".Fill-heart").css("display","block");
+       $(this).css("display","none" );
+      //  $(this).effect("bounce", "slow");
+  })
+
+
+
+  $(".Fill-heart").click(function(){
+    $(".blank-heart").css("display","block");
+    $(this).css("display","none");
+    // $(this).effect("bounce", "slow");
+})
+
+// ---Collapse-1---
+$(".collap-head1").click(function(){
+  $(".Clp-toggle1").slideToggle("");
+  $(this).toggleClass("collape-active");
+})
+
+// ---Collapse-2---
+$(".collap-head2").click(function(){
+  $(".Clp-toggle2").slideToggle("");
+  $(this).toggleClass("collape-active2");
+})
+// ---Collapse---end---here
+
+// ---Collapse-3---
+$(".collap-head3").click(function(){
+  $(".Clp-toggle3").slideToggle("");
+  $(this).toggleClass("collape-active3");
+})
+// ---Collapse3333---end---here
+
+
+// ---Collapse-4444---
+$(".collap-head4").click(function(){
+  $(".Clp-toggle4").slideToggle("");
+  $(this).toggleClass("collape-active4");
+})
+// ---Collapse4444---end---here
+
+// ---Collapse-555---
+$(".collap-head5").click(function(){
+  $(".Clp-toggle5").slideToggle("");
+  $(this).toggleClass("collape-active5");
+})
+// ---Collapse5555---end---here
+// ---Collapse-6666---
+$(".collap-head6").click(function(){
+  $(".Clp-toggle6").slideToggle("");
+  $(this).toggleClass("collape-active6");
+})
+// ---Collapse66665---end---here
+
+// ---Collapse-7777----
+$(".collap-head7").click(function(){
+  $(".Clp-toggle7").slideToggle("");
+  $(this).toggleClass("collape-active7");
+})
+// ---Collapse7777-----end---here
+
+// ---Collapse-8888----
+$(".collap-head8").click(function(){
+  $(".Clp-toggle8").slideToggle("");
+  $(this).toggleClass("collape-active8");
+})
+// ---Collapse8888-----end---here
+
+// ---Collapse-9999----
+$(".collap-head9").click(function(){
+  $(".Clp-toggle9").slideToggle("");
+  $(this).toggleClass("collape-active9");
+})
+// ---Collapse9999-----end---here
 
 
       // ------slider1--
@@ -87,6 +175,27 @@ $(document).ready(function(){
             },
             1000: {
                 items: 5
+            }
+        }
+      });
+       // ------slider4--
+       $('.slider4').owlCarousel({
+        loop: false,
+        margin: 0,
+        // autoplay: true,
+         nav: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1500,
+        animateOut: 'fadeOut',
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
             }
         }
       });
